d4a05320d416d674f984bbc46127290df749cdf1
karnurmax <karnurmax@mail.ru>
2021-12-13 14:54:34 +0600

    add pzu_dolya input

--- web/AisgzkWebWrapper/wwwroot/js/aisgzk/ctrl/pzu.js
+++ web/AisgzkWebWrapper/wwwroot/js/aisgzk/ctrl/pzu.js
@@ -3,12 +3,11 @@ const ctrl = window.aisgzkV2Ctrl.pzu = {};
 
 ctrl.onload = () => {
     console.log('aisgzkV2Ctrl.onload');
-    $('.region-tree-container form').append(
-        `<div class="pu-developer" style="">
+    if (!$('#pzu_dolya').length)
+        $(`<div class="pu-developer" style="" id="pzu_dolya">
             <div class="form-group">
-                <label for="" class="col-form-label-ggg">Площадь (га):</label>
-                <input type="number" id="pu_square" class="form-control shadow-none">
+                <label for="" class="col-form-label-ggg">Площадь доли(га):</label>
+                <input type="number" id="pu_square_dolya" class="form-control shadow-none">
             </div>
-        </div>`
-    );
+        </div>`).insertBefore($('.region-tree-container form button.btn-enter-coordinates').parent());
 }
