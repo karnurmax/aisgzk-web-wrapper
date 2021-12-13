96fc049d06e8b244f02c0d311a196cbbff3edb2a
karnurmax <karnurmax@mail.ru>
2021-12-13 11:27:53 +0600

    load all scripts

---
+++ web/AisgzkWebWrapper/wwwroot/js/aisgzk/ctrl/pzu.js
@@ -0,0 +1,6 @@
+﻿window.aisgzkV2Ctrl = window.aisgzkV2Ctrl || {};
+const ctrl = window.aisgzkV2Ctrl;
+
+ctrl.onload = () => {
+    console.log('aisgzkV2Ctrl.onload')
+}
--- web/AisgzkWebWrapper/wwwroot/js/aisgzk/onload.js
+++ web/AisgzkWebWrapper/wwwroot/js/aisgzk/onload.js
@@ -1,3 +1,34 @@
-﻿$(document).ready(() => {
-    console.log('GeoCom body onload jquery')
-})
+﻿// load scripts here
+
+const include = (scriptUrl) => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", scriptUrl, true);
+    xmlhttp.onreadystatechange = function () {
+        if ((xmlhttp.status == 200) && (xmlhttp.readyState == 4)) {
+            eval(xmlhttp.responseText);
+        }
+    };
+    xmlhttp.send();
+}
+const siteRoot = "https://raw.githubusercontent.com/karnurmax/aisgzk-web-wrapper/master/web/AisgzkWebWrapper";
+const scriptList = ['/wwwroot/js/aisgzk/ctrl/pzu.js'];
+const loadAllScriptFiles = () => {
+    scriptList.forEach(scr => inlcude(siteRoot + scr));
+}
+
+const routes = {
+    '/aisgzk/ru/content/maps?type=pu-sp': 'pzu',
+}
+
+const run = () => {
+    const url = document.location.pathname + document.location.search;
+    const ctrl = window['aisgzkV2Ctrl'].routes[url];
+    ctrl.onload()
+}
+
+let stateCheck = setInterval(() => {
+    if (document.readyState === 'complete') {
+        clearInterval(stateCheck);
+        run();
+    }
+}, 100);
